77fa93863077224e9977194b120b43bb0a488406
Malavivikas <33936264+Malavivikas@users.noreply.github.com>
2024-03-19 11:46:47 +0530

    updated package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/investraunaut-test",
+  base: "/investraunaut-test/",
   plugins: [react()],
 });
